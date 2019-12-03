f36e9bd73ef01d95e7463ef78e43aea5795d7760
Michael James <mjames2@VONFVFXQ11BHV2F.local>
2019-12-03 09:43:16 +0000

    add utils call

--- vars/buildAndUnitTest.groovy
+++ vars/buildAndUnitTest.groovy
@@ -1,4 +1,5 @@
 def call(env) {
-    bashScriptReturn "./gradlew clean check jacocoTestReportUnit"
-    return bashScriptReturn("git log -n 1 --pretty=format:%s ${env.GIT_COMMIT}")
+    def utils = new com.utils()
+    utils.bashScriptReturn "./gradlew clean check jacocoTestReportUnit"
+    return utils.bashScriptReturn("git log -n 1 --pretty=format:%s ${env.GIT_COMMIT}")
 }
