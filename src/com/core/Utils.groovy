e0762a413da01ba253d47985645fa17b8d7e179c
Michael James <mjames2@VONFVFXQ11BHV2F.local>
2019-12-03 10:09:15 +0000

    trying something

--- src/com/core/Utils.groovy
+++ src/com/core/Utils.groovy
@@ -1,18 +1,12 @@
 package com.core
 
 
-class Utils implements Serializable {
-    def steps
+def bashScript(script) {
+    sh "echo IN BUILD SCRIPT"
+    sh "${script}"
+}   
 
-    Utils(steps) {this.steps = steps}
-
-    def bashScript(script) {
-        steps.sh "echo IN BUILD SCRIPT"
-        steps.sh "${script}"
-    }   
-
-    def bashScriptReturn(script) {
-        return steps.sh(returnStdout: true, script:"${script}")
-    }
+def bashScriptReturn(script) {
+    return sh(returnStdout: true, script:"${script}")
 }
 
--- vars/buildAndUnitTest.groovy
+++ vars/buildAndUnitTest.groovy
@@ -1,5 +1,8 @@
+library('Utils')
+import com.core.Utils
+
 def call(env) {
-    def utils = new com.core.Utils(this)
-    utils.bashScriptReturn "./gradlew clean check jacocoTestReportUnit"
-    return utils.bashScriptReturn("git log -n 1 --pretty=format:%s ${env.GIT_COMMIT}")
+    //def utils = new com.core.Utils(this)
+    bashScriptReturn "./gradlew clean check jacocoTestReportUnit"
+    return bashScriptReturn("git log -n 1 --pretty=format:%s ${env.GIT_COMMIT}")
 }
