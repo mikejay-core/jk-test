150dd8171d282a35606868da36eb595ac46539f8
Michael James <mjames2@VONFVFXQ11BHV2F.local>
2019-12-03 09:37:04 +0000

    [CORE-3633] update lib to remove package reference

--- vars/buildAndUnitTest.groovy
+++ vars/buildAndUnitTest.groovy
@@ -1,8 +1,3 @@
-#!/usr/bin/env groovy
-@Library('utils')
-import com.core.utils
-
-
 def call(env) {
     bashScriptReturn "./gradlew clean check jacocoTestReportUnit"
     return bashScriptReturn("git log -n 1 --pretty=format:%s ${env.GIT_COMMIT}")
