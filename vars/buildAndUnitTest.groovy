37bf321f68de15d7c9c3a4fe60cc051b01e91ed8
Michael James <mjames2@VONFVFXQ11BHV2F.local>
2019-12-03 10:10:53 +0000

    extend tests

--- vars/buildAndUnitTest.groovy
+++ vars/buildAndUnitTest.groovy
@@ -3,6 +3,6 @@ import com.core.Utils
 
 def call(env) {
     //def utils = new com.core.Utils(this)
-    bashScriptReturn "./gradlew clean check jacocoTestReportUnit"
-    return bashScriptReturn("git log -n 1 --pretty=format:%s ${env.GIT_COMMIT}")
+    Utils.bashScriptReturn "./gradlew clean check jacocoTestReportUnit"
+    return Utils.bashScriptReturn("git log -n 1 --pretty=format:%s ${env.GIT_COMMIT}")
 }
