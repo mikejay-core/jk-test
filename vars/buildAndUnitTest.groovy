cbdef263302850e3727c324982d377d8e67f4479
Michael James <mjames2@VONFVFXQ11BHV2F.local>
2019-12-03 09:33:01 +0000

    .

--- vars/buildAndUnitTest.groovy
+++ vars/buildAndUnitTest.groovy
@@ -5,5 +5,5 @@ import com.core.libs.utils
 
 def call(env) {
     bashScriptReturn "./gradlew clean check jacocoTestReportUnit"
-    return bashScriptReturn "git log -n 1 --pretty=format:%s ${env.GIT_COMMIT}"
+    return bashScriptReturn("git log -n 1 --pretty=format:%s ${env.GIT_COMMIT}")
 }
