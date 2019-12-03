5dcf5c4ad24660bdecb2ee52e9e8d853c7f3b87c
Michael James <mjames2@VONFVFXQ11BHV2F.local>
2019-12-03 10:00:53 +0000

    add "this"

--- vars/buildAndUnitTest.groovy
+++ vars/buildAndUnitTest.groovy
@@ -1,5 +1,5 @@
 def call(env) {
-    def utils = new com.core.Utils()
+    def utils = new com.core.Utils(this)
     utils.bashScriptReturn "./gradlew clean check jacocoTestReportUnit"
     return utils.bashScriptReturn("git log -n 1 --pretty=format:%s ${env.GIT_COMMIT}")
 }
